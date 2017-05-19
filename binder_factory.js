ec93358718b190eee51dbff1ce2d18890391dd2c
Stéfano Stypulkowski Zanata <made.in.stefano@gmail.com>
2017-05-19 15:18:54 -0300

    Binding was not setting valus if those evaluated to false (eg: 0, or '')

--- binder_factory.js
+++ binder_factory.js
@@ -3,7 +3,7 @@
 */
 
 function getDeepValue(obj, path) {
-  return path.reduce( (ref, key) => ref && ref[key] ? ref[key] : null, obj);
+  return path.reduce( (ref, key) => ref && ref.hasOwnProperty(key) ? ref[key] : null, obj);
 }
 
 function getPaths(obj) {
@@ -11,7 +11,7 @@ function getPaths(obj) {
   if (!obj || obj.constructor.name !== 'Object') {
     return paths;
   }
-  
+
   Object.keys(obj).forEach( key => {
     var sub = getPaths( obj[key] );
     if (sub.length > 0) {
@@ -20,7 +20,7 @@ function getPaths(obj) {
       paths.push(key);
     }
   });
-  
+
   return paths.map( v => v.split('.') );
 }
 
@@ -42,35 +42,35 @@ function setDeepValue(obj, path, value) {
 }
 
 module.exports = {
-  
+
   /**
   * @param {...string} - All bindable fields to this binder
   * @return {{bind: function}} - A new binder object
   */
   build() {
-    
+
     var all = false, paths = [];
     if (arguments[0] === '*') {
       all = true;
     } else {
       paths = [...arguments].map( v => v.split('.') );
     }
-    
+
     return {
-      
+
       /**
       * @param {Object} obj1 - object tor receive the values
       * @param {Object} obj1 - object to get the values, according to the fields
       */
       bind(obj1, obj2) {
-        
-        if (all) {  
+
+        if (all) {
           getPaths(obj1).forEach( path => {
             setDeepValue(obj1, path, getDeepValue(obj2, path) );
           });
-          
+
         } else {
-          
+
           paths.forEach( path => {
             setDeepValue(obj1, path, getDeepValue(obj2, path) );
           });
